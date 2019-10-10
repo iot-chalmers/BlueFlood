33cfde6b6b2b498686f352f4bd1645ad5eb0787b
Beshr Al Nahas <beshr@chalmers.se>
2019-10-10 15:02:26 +0200

    minor: add one node to CAU testbed

--- examples/nrf-glossy/testbed.h
+++ examples/nrf-glossy/testbed.h
@@ -64,9 +64,6 @@
 
 #elif TESTBED==CAU_TESTBED
 
-// #define TESTBED_IDS (uint32_t[]){0x43c18ccc, 0xb4fddd93, 0x27e5a59}
-// #define TESTBED_PI_IDS (uint8_t[]){3,4,5}
-
 // 1       #@ Sep 10 2019 16:58:01, ID: 0xa8d4c97d, 
 // 2       #@ Sep 10 2019 16:58:01, ID: 0x921a4434, 
 // 3       #@ Sep 10 2019 16:58:01, ID: 0x43c18ccc, 
@@ -86,10 +83,10 @@
 // 18      #@ Sep 10 2019 16:58:01, ID: 0xe7c767e3, 
 // 19      #@ Sep 10 2019 16:58:01, ID: 0x1a36886, 
 // 20      #@ Sep 10 2019 16:58:01, ID: 0x59c34e8f, 
+// 21      #@ Oct 10 2019         , ID: 0xbda9a2e0, 
 
-
-#define TESTBED_IDS (uint32_t[]){0xa8d4c97d,0x921a4434,0x43c18ccc, 0xb4fddd93, 0x27e5a59,0x8036b5ea,0x4a8f1926,0x39f7b9e3,0xd73c5005,0x7dd93314,0x9fb4c897,0x22087f8e,0x9463fd32,0xece2f14c,0x1add226c,0xbd08d46b,0xe7c767e3,0x1a36886,0x59c34e8f}
-#define TESTBED_PI_IDS (uint8_t[]){1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19,20}
+#define TESTBED_IDS (uint32_t[]){0xa8d4c97d,0x921a4434,0x43c18ccc, 0xb4fddd93, 0x27e5a59,0x8036b5ea,0x4a8f1926,0x39f7b9e3,0xd73c5005,0x7dd93314,0x9fb4c897,0x22087f8e,0x9463fd32,0xece2f14c,0x1add226c,0xbd08d46b,0xe7c767e3,0x1a36886,0x59c34e8f,0xbda9a2e0}
+#define TESTBED_PI_IDS (uint8_t[]){1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19,20,21}
 #define TESTBED_SIZE sizeof(TESTBED_PI_IDS)
 
 #elif TESTBED==CTH_TESTBED
