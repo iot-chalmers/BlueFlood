cd0c4cef9e7e73eb494e3e9b81628aef62988acf
Beshr Al Nahas <beshr@chalmers.se>
2019-10-15 14:29:13 +0200

    enlarge slot processing time to 75us

--- examples/nrf-glossy/nrf-radio-driver.h
+++ examples/nrf-glossy/nrf-radio-driver.h
@@ -32,7 +32,7 @@
 #define MY_ADV_ADDRESS_HI 0xB0B0U
 #endif
 /*---------------------------------------------------------------------------*/
-#define SLOT_PROCESSING_TIME US_TO_RTIMERTICKS(50)
+#define SLOT_PROCESSING_TIME US_TO_RTIMERTICKS(75)
 #define GUARD_TIME_SHORT (US_TO_RTIMERTICKS(0))
 #define GUARD_TIME (US_TO_RTIMERTICKS(32))
 /*---------------------------------------------------------------------------*/
